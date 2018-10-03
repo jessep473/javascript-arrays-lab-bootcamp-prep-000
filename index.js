83fda35a7eaff5f24b0dd4bee8b8de3db603e5fc
Jesse Peterson <jessep473@gmail.com>
2018-10-03 22:09:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions and code here
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(array, name) {
+  array.shift(name);
+
+}
+
+destructivelyAppendKitten(kittens, Ralph);
